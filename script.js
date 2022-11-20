b6b29d2b19288430563e1a4a59a03e7f886a140a
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-20 17:24:41 +0100

    handling events

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 'use strict';
 //MANIPULATING TEXT
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number';
 
-//MANIPULATING NUMBERS
-document.querySelector('.number').textContent = '13';
-document.querySelector('.score').textContent = '50';
+// //MANIPULATING NUMBERS
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.score').textContent = '50';
 
-//MANIULATING AN IPNUT FIELD
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// //MANIULATING AN IPNUT FIELD
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//EVENT LISTENER
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'no Number';
+  }
+});
