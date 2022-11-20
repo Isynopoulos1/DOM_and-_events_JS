2c16118ddaa5cf2417dbb6ce158ecda15d5be5a4
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-20 16:32:53 +0100

    test2

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '50';
 
 //MANIULATING AN IPNUT FIELD
-console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
