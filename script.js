538fdb414c4a68ea6ed0d5606d8c52436a4610ee
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-21 12:00:52 +0100

    implementing the game logic part 2

--- script.js
+++ script.js
@@ -26,12 +26,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'CORRECT NUMBER!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'TOO HIGH!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'TOO HIGH!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'TOO LOW!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'TOO LOW!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
