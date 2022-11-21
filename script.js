da8f6d92f62911e81a25ab1eca9d503cf7d609bb
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-21 11:51:13 +0100

    implementing the game logic

--- script.js
+++ script.js
@@ -12,11 +12,26 @@
 // console.log(document.querySelector('.guess').value);
 
 //EVENT LISTENER
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'no Number';
+    document.querySelector('.message').textContent = 'NO NUMBER';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'CORRECT NUMBER!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'TOO HIGH!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'TOO LOW!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
